4787611f5cd112bd93c19bdefef16e661a9a32e8
alksndr11 <145945676+alksndr11@users.noreply.github.com>
2024-03-06 01:29:37 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                  help="Choose language: en or ru")
